e4e8ad7b79035d5c7bc8a9eee91311165d1dc922
Skott Klebe <skottk@gmail.com>
2012-10-25 19:33:44 -0400

    now totally template-driven
    Signed-off-by: Skott Klebe <skottk@gmail.com>

--- src/torrents/core.clj
+++ src/torrents/core.clj
@@ -9,64 +9,26 @@
    by enlive."
   [url] (html/html-resource (java.net.URL. url)))
 
-(defn get-award-links [url]
-  (map :attrs (html/select (fetch-url url)
-                    #{[:div#content :li.page_item :a]
-                      [:li.page.item.subtext html/first-child]})))
-
-
-(def tpb-tpl
-  { :url "http://thepiratebay.se/browse/601/0/9"
-    :name [:table#searchResult :tr html/first-child]})
-
-(defn fetch-tpl [nodes]
-  (let [titles (html/select nodes
-                            #{[:table#searchResult :td :a.detLink ]} )]
-    (map (juxt
-          #(get-in %1 [:attrs :title])
-          #(get-in %1 [:attrs :href]))
-         titles
-         )))
-
-(defn nth-indexed [idxs s]
-  (map #(get %1 s)idxs ))
-
-
-(defn get-indexed-tags [ row tag idxs]
-  (map #(get-col-by-tag row tag %)
-       idxs))
-
-
-
-
-(def columns {
-             ; :work [ 2 :a]
-              :seeders [ 5 :content]
-              :leechers [ 7 :content] })
-
-(defn get-col-by-tag [row  tag idx]
-  (tag (nth row idx)))
-
-(defn get-tagged-cols [ row cols]
-  (merge (map #(assoc {}
-                  (key %)
-                  (get-col-by-tag row
-                                  (second (val %))
-                                  (first (val %))) )
-               cols)))
-
-
-(defn get-keyed-tags [row tag idxs]
-  (map #( tag ( nth row %))  idxs))
-
 (defn get-tpb-rows [nodes]
   (butlast (rest (html/select nodes  #{[:table#searchResult :tr ]} ))))
 
-(pprint
- (map
-  #( get-tagged-cols % columns)
-  (get-tpb-rows nodes)))
+(def tpb-tpl
+  { :url "http://thepiratebay.se/browse/601/0/9"
+   :name [:table#searchResult :tr html/first-child]
+   :columns {:work #(html/select
+                  % #{[( html/nth-of-type 2) :a html/content]} )
+          :seeders #(html/select
+                     % #{[( html/nth-of-type 3) html/content]} )
+          :leechers #(html/select
+                      % #{[( html/nth-of-type 4) html/content]} ) }})
 
-(pprint  (map
-          #( get-tagged-cols % columns)
-          (get-tpb-rows nodes)))
+(defn get-tagged-cols [ row cols]
+  (into {} (map #( vector
+                  (key %)
+                  (first ((val %) row)) )
+               cols)))
+
+(defn parse-tpb-page [tpl]
+  (let [nodes (fetch-url (:url tpl))
+        rows (get-tpb-rows nodes)]
+    (map  #(get-tagged-cols % (:columns tpl)) rows)))
